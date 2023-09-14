34276a31fc2ab488e9817e6b136db41e6e52856a
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-14 16:08:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function hellocode()
+{
+let divelement = document.getElementById("browser-info");
+	console.log(divelement);
+let ptag = document. createElement("p");
+ptag.innerText = "You are using " + navigator.appName +" "+"version" + navigator.appVersion
+divelement.appendChild(ptag);
+}
